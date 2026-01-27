466e7b0114b1a500bdb7901b79bad4545e17efae
Rapha <git@rappublic.org>
2026-01-27 17:03:05 +0100

    * add info about calling the raycasting interface functions

--- src/main.c
+++ src/main.c
@@ -12,6 +12,18 @@ int main(int argc, char **argv)
     
     if (process_map(data, argv[1]) == false)
         return (1);
-    
-    return (0);
+
+	// Interface functions to raycast section
+	/* Somewhere here, after collection the info from the map:
+	 * (maybe in a subfunction of course)
+	 * init_player_field and cdir could but don't have to be stored somewhere in data,
+	 */
+	// set_initial_player_pos(&data->player, t_fidx init_player_field, t_cdir cdir);
+
+	/* ... and then later in each update loop after evaluating the user input
+	*/
+	// update_player_pos(
+	// 	&data->player, t_coord const deltapos, double const deltadov);
+
+	return (0);
 }
