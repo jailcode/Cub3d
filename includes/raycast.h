ec29ca59c397db28390031c684bfa587a20b5c5e
Rapha <git@rappublic.org>
2026-01-27 16:04:36 +0100

    * implement interface functions   + set_initial_player_pos   + update_player_pos * move all struct/enum defs to separate header * some rfa for clarity

---
+++ includes/raycast.h
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycast.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphha <raphha@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/21 09:41:15 by raphha            #+#    #+#             */
+/*   Updated: 2026/01/27 16:00:15 by raphha           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RAYCAST_H
+# define RAYCAST_H
+
+# ifndef M_PI
+#  define M_PI 3.14159265358979323846
+# endif
+
+# include <stdbool.h>
+
+typedef enum e_compassdir
+{
+	North, East, South, West, cdir_error
+}	t_cdir;
+
+
+typedef enum e_fieldtype
+{
+	wall,
+	empty,
+}	t_fieldtype;
+
+typedef struct s_field
+{
+	t_fieldtype ftype;
+}	t_field;
+
+typedef struct s_fieldindex
+{
+	int horizontal;
+	int vertical;
+}	t_fidx;
+
+// rad: dov angle in rad
+// x,y: Direction of view representation
+//      as normalised vector
+typedef struct s_direction
+{
+	double	rad;
+	double	x;
+	double	y; 
+}	t_dir;
+
+// x in width direction (left->right)
+// y in height direction (top->down)
+typedef struct s_coordinate
+{
+	double x;
+	double y;
+}	t_coord;
+
+typedef struct s_line
+{
+	t_coord	origin;
+	t_dir	dir;
+}	t_line;
+
+// fov: fixed value in rad, typically 60deg 
+// dov: direction of view
+// pos: exact double coordinates of current position
+//      angle from x-axis in clockwise direction
+typedef struct s_player
+{
+	double	fov;
+	t_coord	pos;
+	t_dir	dov;
+}	t_player;
+
+typedef struct s_gridlines
+{
+	t_line	horizontal;
+	t_line	vertical;
+}	t_gridlns;
+
+// the field index is given as a struct with 2 integers,
+// cdir is the compass-direction like N(orth) etc.
+// starting at 0,0 in the top left corner of the map
+bool	set_initial_player_pos(t_player *p, t_fidx init_player_field, t_cdir cdir);
+
+// DeltaDOV is positive in clockwise direction
+// for now the deltadov is in rad, but we could
+// agree to use deg if you prefer
+// The deltapos is a relative distance:
+//   x => to the front/back (positive to the front)
+//   y => to the side (positive to the right)
+int	update_player_pos(
+	t_player *p, t_coord const deltapos, double const deltadov);
+
+#endif // RAYCAST_H
--- src/raycast.c
+++ src/raycast.c
@@ -3,80 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   raycast.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rhaas <rhaas@student.42berlin.de>          +#+  +:+       +#+        */
+/*   By: raphha <raphha@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 09:41:15 by raphha            #+#    #+#             */
-/*   Updated: 2026/01/22 17:27:35 by rhaas            ###   ########.fr       */
+/*   Updated: 2026/01/27 13:30:16 by raphha           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
-typedef enum e_fieldtype
-{
-	wall,
-	empty,
-}	t_fieldtype;
 
-typedef struct s_field
-{
-	t_fieldtype ftype;
-	// t_coord		pos_idx;
-}	t_field;
-
-typedef struct s_map
-{
-	// width and height in terms of map fields
-	// as parsed from input file (not image pixels)
-	int	width; // == no. of block columns
-	int height;
-	
-	t_field	**fields;
-}	t_map2;
-
-typedef struct s_fieldindex
-{
-	int horizontal;
-	int vertical;
-}	t_fidx;
-
-typedef struct s_direction
-{
-	double	rad; // dov angle in rad
-	// Direction of view representation
-	// as normalised vector
-	double	x;
-	double	y; 
-}	t_dir;
-
-typedef struct s_coordinate
-{
- 	// x in width direction (left->right)
- 	// y in height direction (top->down)
-	// pixel for player position, field indices for field??
-	double x;
-	double y;
-}	t_coord;
-
-typedef struct s_line
-{
-	t_coord	ref;
-	t_dir	dir;
-}	t_line;
-
-typedef struct s_player
-{
-	double	fov;
-	t_coord	pos;
-	t_dir	dov; // direction of view; angle from x-axis in clockwise direction;
-}	t_player;
-
-typedef struct s_gridlines
-{
-	t_line	horizontal;
-	t_line	vertical;
-}	t_gridlns;
+#include "cub.h"
+#include "raycast.h"
 
 #include <stdlib.h>
 #include <math.h>
@@ -85,10 +24,10 @@ typedef struct s_gridlines
 t_coord	intersection(t_line const *const rayln, t_line const *const gridln)
 {
 	double const lambda = (
-		  rayln->dir.x * (rayln->ref.y - gridln->ref.y)
-		- rayln->dir.y * (rayln->ref.x - gridln->ref.x)
+		  rayln->dir.x * (rayln->origin.y - gridln->origin.y)
+		- rayln->dir.y * (rayln->origin.x - gridln->origin.x)
 	) / (rayln->dir.x * gridln->dir.y - rayln->dir.y * gridln->dir.x);
-	t_coord const intersect = (t_coord) { gridln->ref.x + lambda * gridln->dir.x, gridln->ref.y + lambda * gridln->dir.y };
+	t_coord const intersect = (t_coord) { gridln->origin.x + lambda * gridln->dir.x, gridln->origin.y + lambda * gridln->dir.y };
 	return (intersect);
 }
 
@@ -96,9 +35,47 @@ double dist(t_coord const *const c1, t_coord const *const c2)
 {
 	return (hypot(c1->x - c2->x, c1->y -c2->y));
 }
+
+bool set_initial_player_pos(t_player *p, t_fidx init_player_field, t_cdir const compassdir)
+{
+	if (compassdir == (t_cdir)North)
+		p->dov.rad = 1.5 * M_PI;
+	else if (compassdir == (t_cdir)East)
+		p->dov.rad = 0.0 * M_PI;
+	else if (compassdir == (t_cdir)South)
+		p->dov.rad = 0.5 * M_PI;
+	else if (compassdir == (t_cdir)West)
+		p->dov.rad = 1.0 * M_PI;
+	else if (init_player_field.horizontal < 0 || init_player_field.vertical < 0)
+		return (false);
+	p->dov.x = cos(p->dov.rad);
+	p->dov.y = sin(p->dov.rad);
+	p->pos.x = (double)init_player_field.horizontal + 0.5;
+	p->pos.y = (double)init_player_field.vertical + 0.5;
+	return (true);
+}
+
+int	update_player_pos(
+	t_player *p, t_coord const deltapos, double const deltadov)
+{
+	p->pos.x += deltapos.x * cos(p->dov.rad) + deltapos.y * sin(p->dov.rad);
+	p->pos.y += deltapos.x * sin(p->dov.rad) + deltapos.y * cos(p->dov.rad);
+	p->dov.rad += deltadov;
+	p->pos.x = cos(p->dov.rad);
+	p->pos.y = sin(p->dov.rad);
+}
+
+bool	is_wall(t_map const *const pmap, t_fidx const fidx)
+{
+	t_field const *const pfield = &pmap->fields[fidx.horizontal][fidx.vertical];
+	
+	return (pfield->ftype == (t_fieldtype)wall);
+}
+
+
 int	main(int argc, char *argv[])
 {
-	t_map2		map;
+	t_map		map;
 	t_player	p = (t_player){.fov = 60.0};
 	
 	map.fields = calloc(map.width, sizeof(t_field *));
@@ -108,25 +85,20 @@ int	main(int argc, char *argv[])
 
 	/* Player init */
 	// idxposition = field idx in map matrix
-	t_coord idxposition; // initialised to initial player idxpos on the map
+	t_fidx	idxposition; // initialised to initial player idxpos on the map
 	char	compassdov;	 // initial dov (N,E,S,W)
-	
-	set_initial_player_pos(t_fidx init_player_field, char dir);
-	update_player_pos(double deltadist, double relangleofmovement, double deltadov);
 
-
-	set_player_posdov(&p, idxposition, compassdov);
-
+	set_initial_player_pos(&p, idxposition, compassdov);
 	int	pixelwidth; // = image.width in pxl
 	t_gridlns	gridlines = {
-		.horizontal = (t_line){.ref = (t_coord){0.0, 0.0}, .dir = (t_dir){.rad = M_PI / 2, .x = 1.0, .y = 0.0}},
-		.vertical =  (t_line){.ref = (t_coord){0.0, 0.0}, .dir = (t_dir){.rad = M_PI / 2, .x = 0.0, .y = 1.0}},
+		.horizontal = (t_line){.origin = (t_coord){0.0, 0.0}, .dir = (t_dir){.rad = M_PI / 2, .x = 1.0, .y = 0.0}},
+		.vertical =  (t_line){.origin = (t_coord){0.0, 0.0}, .dir = (t_dir){.rad = M_PI / 2, .x = 0.0, .y = 1.0}},
 	};
 	
 	for (int idx = 0; idx < pixelwidth; ++idx)
 	{
 		t_line	ray;
-		ray.ref = p.pos;
+		ray.origin = p.pos;
 		double const rayangle = (p.dov.rad - p.fov / 2.0) + idx * (p.fov / (pixelwidth - 1));
 		ray.dir = (t_dir){
 			.rad = rayangle,
@@ -134,13 +106,13 @@ int	main(int argc, char *argv[])
 			.y = sin(rayangle),
 		};
 
-		gridlines.vertical.ref.x = (int)(p.pos.x);
+		gridlines.vertical.origin.x = (int)(p.pos.x);
 		if (ray.dir.x > 0)
-			gridlines.vertical.ref.x += 1.0;
+			gridlines.vertical.origin.x += 1.0;
 
-		gridlines.horizontal.ref.y = (int)(p.pos.y);
+		gridlines.horizontal.origin.y = (int)(p.pos.y);
 		if (ray.dir.y > 0)
-			gridlines.horizontal.ref.y += 1.0;
+			gridlines.horizontal.origin.y += 1.0;
 
 		bool wall_found = false;
 		t_fidx fieldidx;
@@ -149,8 +121,8 @@ int	main(int argc, char *argv[])
 		{
 			t_coord intersect_horizontal = intersection(&ray, &gridlines.horizontal);
 			t_coord intersect_vertical = intersection(&ray, &gridlines.vertical);		
-			double dist_h = dist(&ray.ref, &intersect_horizontal);
-			double dist_v = dist(&ray.ref, &intersect_vertical);
+			double dist_h = dist(&ray.origin, &intersect_horizontal);
+			double dist_v = dist(&ray.origin, &intersect_vertical);
 
 			intersect = intersect_vertical;
 			bool hitvertical = true;
@@ -167,11 +139,11 @@ int	main(int argc, char *argv[])
 			 	--fieldidx.horizontal;
 			if (ray.dir.y < 0)
 			 	--fieldidx.vertical;
-			if (is_wall(fieldidx))
+			if (is_wall(&map, fieldidx))
 			{
 				wall_found = true;
 			}
 		}
-		
+		printf("Intersection for ray angle\n");
 	}
 }
